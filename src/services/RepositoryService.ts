041766480d8a7cc8dbbdbb0a368ab6326f580b73
nayemalam <nayem.dev@gmail.com>
2024-05-05 22:41:46 -0400

    feat: add bearer token

--- src/services/RepositoryService.ts
+++ src/services/RepositoryService.ts
@@ -1,4 +1,4 @@
-import { API_BASE_URL } from './api';
+import { API_BASE_URL, accessToken } from './api';
 
 export async function searchRepositories(
   text: string,
@@ -17,6 +17,12 @@ export async function searchRepositories(
     // https://api.github.com/search/repositories?q=SmartPaperSearch&order=desc&page=1&per_page=10
     const response = await fetch(
       `${API_BASE_URL}/search/repositories?${queryParams}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
     );
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
@@ -31,7 +37,12 @@ export async function searchRepositories(
 
 export const getRepository = async (owner: string, repo: string) => {
   try {
-    const response = await fetch(`${API_BASE_URL}/repos/${owner}/${repo}`);
+    const response = await fetch(`${API_BASE_URL}/repos/${owner}/${repo}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
@@ -50,6 +61,12 @@ export const getLanguagesByOwnerAndRepo = async (
   try {
     const response = await fetch(
       `${API_BASE_URL}/repos/${owner}/${repo}/languages`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
     );
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,1 +1,3 @@
 export const API_BASE_URL = 'https://api.github.com';
+
+export const accessToken = process.env.EXPO_PUBLIC_ACCESS_TOKEN;
